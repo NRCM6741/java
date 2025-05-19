0e190e2af2d2eb4ba1c9c6ef0befb779ce7f0e49
NRCM6741 <meenapallyarchana@gmail.com>
2025-05-19 14:12:19 +0530

    hi

---
+++ Nrcm/src/Day_2/Set10.java
@@ -0,0 +1,13 @@
+package Day_2;
+import java.util.*;
+
+public class Set10 {
+
+	public static void main(String[] args) throws ArithmeticException {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a/b;
+		System.out.println(c);
+	}
+}
---
+++ Nrcm/src/Day_2/Set12.java
@@ -0,0 +1,20 @@
+package Day_2;
+import java.util.*;
+
+public class Set12 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		try {
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int c=a/b;
+			System.out.println(c);
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+}
